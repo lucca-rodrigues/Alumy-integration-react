18f9818e5263210cb03079150fe7908b22db66ae
Lucas Rodrigues <contato.luccarodrigues@gmail.com>
2022-01-17 20:44:29 -0300

    update styles from top relateds and generanew new build

--- src/Components/CustomBg/styles.js
+++ src/Components/CustomBg/styles.js
@@ -4,10 +4,9 @@ import { withStyles } from "@mui/styles";
 export const Container = withStyles((theme) => ({
   root: {
     backgroundColor: theme.colors.black,
-    padding: "0 0px 50px 50px",
+    padding: "30px 0px 50px 50px",
     color: theme.colors.white,
     boxShadow: `9px -103px 83px 14px ${theme.colors.black}`,
     height: "100wh",
-    paddingBottom: 50,
   },
 }))(Grid);
